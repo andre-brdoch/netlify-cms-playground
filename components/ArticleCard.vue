00eda521be85e660f930012a301247fb03ade340
andrekisi <andre@kisi.io>
2020-09-26 21:26:20 +0200

    ArticleCard.vue

---
+++ components/ArticleCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <article>
+    <nuxt-link
+      :to="href"
+      class="bg-white block flex flex-col h-full overflow-hidden rounded-md shadow-lg">
+      <div
+        class="bg-center bg-cover bg-no-repeat"
+        :style="`background-image: url('${img}');`">
+        <div class="pt-2/3" />
+      </div>
+      <div class="flex flex-col flex-grow justify-between p-3">
+        <div>
+          <h3 class="font-bold text-lg text-neutral-800">
+            {{ label }}
+          </h3>
+          <p class="mt-2 text-neutral-800 text-sm tracking-tight">
+            {{ text }}
+          </p>
+        </div>
+        <p class="mt-4 text-neutral-800 text-right text-sm tracking-tight">
+          {{ date }}
+        </p>
+      </div>
+    </nuxt-link>
+  </article>
+</template>
+
+<script>
+export default {
+  props: {
+    href: { type: String, required: true },
+    img: { type: String, required: true },
+    label: { type: String, required: true },
+    text: { type: String },
+    date: { type: String },
+  },
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,68 @@
 <template>
   <div>
-    <h1>
-      netlify-cms-playground
-    </h1>
-    <button
-      class="bg-primary-500 font-bold font-standard hover:bg-primary-400 px-4 py-2 rounded text-white">
-      Button
-    </button>
+    <header>
+      <div class="container md:px-0 px-5">
+        <h1 class="font-bold leading-tight mb-4 md:text-6xl text-4xl">
+          netlify-cms-playground
+        </h1>
+      </div>
+    </header>
+    <section class="py-4">
+      <div class="container md:px-0 px-5">
+        <div class="gap-5 grid grid-cols-1 lg:grid-cols-3 sm:grid-cols-2">
+          <ArticleCard
+            v-for="article in articles"
+            :key="article.label"
+            v-bind="article" />
+        </div>
+      </div>
+    </section>
   </div>
 </template>
+
+<script>
+import ArticleCard from '@/components/ArticleCard';
+
+export default {
+  components: { ArticleCard },
+
+  asyncData() {
+    return {
+      articles: [
+        {
+          label: 'Test',
+          img:
+            'https://i2.wp.com/movingtips.wpengine.com/wp-content/uploads/2020/04/mt-crested-butte-colorado.jpg',
+          text: 'This is a text',
+          date: 'Jan 13 1995',
+          href: '/',
+        },
+        {
+          label: 'Hello World',
+          img:
+            'https://i2.wp.com/movingtips.wpengine.com/wp-content/uploads/2020/04/mt-crested-butte-colorado.jpg',
+          text: 'This is a text',
+          date: 'Jan 13 1995',
+          href: '/',
+        },
+        {
+          label: 'This is a the longest text of them all',
+          img:
+            'https://i2.wp.com/movingtips.wpengine.com/wp-content/uploads/2020/04/mt-crested-butte-colorado.jpg',
+          text: 'This is a text',
+          date: 'Jan 13 1995',
+          href: '/',
+        },
+        {
+          label: 'This is a longer text',
+          img:
+            'https://i2.wp.com/movingtips.wpengine.com/wp-content/uploads/2020/04/mt-crested-butte-colorado.jpg',
+          text: 'This is a text',
+          date: 'Jan 13 1995',
+          href: '/',
+        },
+      ],
+    };
+  },
+};
+</script>
