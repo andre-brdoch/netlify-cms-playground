128c5c723e1a792bbea266b8a4e150a591d533b4
andrekisi <andre@kisi.io>
2020-09-26 15:40:24 +0200

    nav

---
+++ components/TheNav.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav class="bg-primary-500 border-b-8 border-primary-700 py-3">
+    <div class="container container-xl flex items-center justify-between">
+      <div class="bg-primary-300 h-12 w-16" />
+      <ul class="flex items-center">
+        <li v-for="item in linkItems" :key="item.label" class="pl-5">
+          <a class="font-bold text-2xl text-primary-800" :href="item.href">{{
+            item.label
+          }}</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: {
+    linkItems: {
+      type: Array,
+      default: () => [],
+      validator: arr => arr.every(item => [ 'href', 'label' ].every(key => key in item)),
+    },
+  },
+};
+</script>
