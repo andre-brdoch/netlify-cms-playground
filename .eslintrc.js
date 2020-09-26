0b651784ac5bf03f4293e81528f95cb4aaa21ec5
andrekisi <andre@kisi.io>
2020-09-26 03:19:48 +0200

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  root: true,
+  env: { browser: true, node: true },
+  extends: [ '@andre-brdoch/eslint-config-vueslint' ],
+};
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,33 +1,47 @@
-import fs from "fs";
-import { join } from "path";
+import fs from 'fs';
+import { join } from 'path';
 
-const HTTPS_KEY = join(__dirname, "credentials/ca.key");
-const HTTPS_CERT = join(__dirname, "credentials/ca.cert");
+const HTTPS_KEY = join(__dirname, 'credentials/ca.key');
+const HTTPS_CERT = join(__dirname, 'credentials/ca.cert');
 
 export default {
-  target: "static",
+  target: 'static',
 
   head: {
-    title: "netlify-cms-playground",
+    title: 'netlify-cms-playground',
     meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" }
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    link: [ { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' } ],
   },
 
   components: true,
 
-  buildModules: ["@nuxtjs/tailwindcss"],
+  buildModules: [ '@nuxtjs/tailwindcss' ],
 
   server: {
     https:
-      process.env.NODE_ENV === "development"
-        ? {
-            key: fs.readFileSync(HTTPS_KEY, "ascii"),
-            cert: fs.readFileSync(HTTPS_CERT, "ascii")
-          }
-        : null
-  }
+      process.env.NODE_ENV === 'development' ?
+        {
+          key: fs.readFileSync(HTTPS_KEY, 'ascii'),
+          cert: fs.readFileSync(HTTPS_CERT, 'ascii'),
+        } :
+        null,
+  },
+
+  build: {
+    extend(config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/,
+        });
+      }
+    },
+  },
 };
