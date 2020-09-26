c226ee0edd349410cf0e9f60bad004974d99ce6e
andrekisi <andre@kisi.io>
2020-09-26 20:53:39 +0200

    nav: nuxt-link

--- components/TheNav.vue
+++ components/TheNav.vue
@@ -1,12 +1,14 @@
 <template>
   <nav class="bg-primary-500 border-b-8 border-primary-700 py-3">
     <div class="container flex items-center justify-between">
-      <div class="bg-primary-300 h-12 w-16" />
+      <nuxt-link to="/" class="bg-primary-300 block h-12 w-16" />
       <ul class="hidden items-center sm:flex">
         <li v-for="item in linkItems" :key="item.label" class="pl-5">
-          <a class="font-bold text-2xl text-primary-800" :href="item.href">{{
-            item.label
-          }}</a>
+          <nuxt-link
+            class="font-bold text-2xl text-primary-800"
+            :to="item.href">
+            {{ item.label }}
+          </nuxt-link>
         </li>
       </ul>
     </div>
